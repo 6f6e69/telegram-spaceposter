77a33c150b6421b2427cb0064b3220b9d4319f42
Alexander Mityukov <a.m.mityukov@gmail.com>
2022-12-18 13:54:57 +0100

    publish photo to Telegram channel

--- telegram_post_bot.py
+++ telegram_post_bot.py
@@ -1,8 +1,18 @@
 from environs import Env
 import telegram
+import pathlib
+import random
+
+
+def get_random_image_path(path: str) -> str:
+    images = list(pathlib.Path(path).iterdir())
+    return str(random.choice(images))
+
 
 if __name__ == '__main__':
     env = Env()
     env.read_env()
     bot = telegram.Bot(token=env('TG_BOT_KEY'))
-    bot.send_message(text='Hello!', chat_id=env('TG_GROUP_ID'))
+    chat_id = env('TG_GROUP_ID')
+    random_image_path = get_random_image_path('images')
+    bot.send_photo(chat_id=chat_id, photo=open(random_image_path, 'rb'))
